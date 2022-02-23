a471458fc7741638f5e5bb3422346cfc96acf404
Charanjit Singh <bitspook@protonmail.com>
2022-02-23 20:57:54 +0530

    feat: ability to save browser tabs as org-subtree
    Also,
    feat: ability to convert browser-tabs saved as org-subtree to a list of plists

--- spookfox.el
+++ spookfox.el
@@ -21,12 +21,11 @@
 (defvar sf--last-action-output nil
   "Output produced by last spookfox action.")
 
-(defvar spookfox-archived-tabs-target nil
+(defvar spookfox-saved-tabs-target nil
   "Target parse-able by org-capture-template where archived tabs will be saved.")
 
 (defun sf--process-output-filter (process output)
   "Save OUTPUT of last action sent to spookfox PROCESS.
-
 For now let's only keep track of the last message. If it the
 communication b/w browser and Emacs gets noisier, we'll introduce
 a request-id system to keep track of requests and their
@@ -51,7 +50,6 @@ responses, and maintain a data structure."
 
 (defun sf--send-message (msg)
   "Send a MSG to browsers connected with spookfox.
-
 MSG will be json encoded before sending."
   (when (or
          (not sf--connection)
@@ -61,7 +59,6 @@ MSG will be json encoded before sending."
 
 (defun sf--get-last-message (&optional call-count)
   "Synchronously provide latest message received from browser.
-
 Returns a plist obtained be decoding incoming message. Since
 socket-communication with spookfox is async, this function blocks
 Emacs for 1 second. If it don't receive a response in that time,
@@ -94,22 +91,54 @@ exit condition in recursive re-checks."
   (sf--send-action "GET_ALL_TABS")
   (sf--get-last-message))
 
-(defun sf--tabs-to-org-nodes (tabs)
-  "Convert spookfox browser TABS to level 1 org-subtrees."
-  (mapconcat
-   (lambda (tab)
-     (concat "* " (plist-get tab :title) "\n:PROPERTIES:\n:URL:\t" (plist-get tab :url) "\n:END:\n"))
-   tabs))
+(defun sf--serialize-tab (tab)
+  "Convert browser TAB to org-mode-subtree string."
+  (concat "* "
+          (plist-get tab :title)
+          "\n:PROPERTIES:\n"
+          ":TAB_ID:\t" (format "%d" (plist-get tab :id)) "\n"
+          ":URL:\t" (plist-get tab :url) "\n"
+          ":END:\n"))
+
+(defun sf--deserialize-tab ()
+  "Return browser tab for subtree at point.
+This function is useful for `org-map-entries`."
+  (let ((props (org-entry-properties)))
+    `(:title ,(alist-get "ITEM" props nil nil #'string=)
+      :url ,(alist-get "URL" props nil nil #'string=)
+      :tabId ,(alist-get "TAB_ID" props nil nil #'string=))))
 
 (defun sf--save-tabs ()
   "Save spookfox tabs as an `org-mode` subtree.
-
 Tabs subtree is saved in `spokfox-archived-tabs-target`"
   (let* ((tabs (sf--get-all-tabs))
-         (tabs-subtree (sf--tabs-to-org-nodes tabs)))
-    (org-capture-set-target-location spookfox-archived-tabs-target)
+         (tabs-subtree (seq-mapcat #'sf--serialize-tab tabs 'string)))
+    (org-capture-set-target-location spookfox-saved-tabs-target)
     (org-capture-put :template tabs-subtree)
     (org-capture-place-template)))
+
+(defun sf--get-saved-tabs ()
+  "Get browser tabs saved with spookfox.
+Returns a list of tabs as plists. Any subtree which don't have a
+TAB_ID is discarded."
+  (let ((tabs nil))
+    (org-capture-set-target-location spookfox-saved-tabs-target)
+    (save-excursion
+      (with-current-buffer (org-capture-get :buffer)
+        (goto-char (org-capture-get :pos))
+        (org-narrow-to-subtree)
+        (setq tabs (org-map-entries #'sf--deserialize-tab))
+        (widen)))
+    (seq-filter (lambda (tab) (plist-get tab :tabId)) tabs)))
+
+;; Public interface
+(defun spookfox-save-all-tabs ()
+  "Save all currently open browser tabs at `spookfox-saved-tabs-target`.
+It will open a capture buffer so user get a chance to preview and
+make changes."
+  (interactive)
+  (sf--save-tabs))
+
 
 (provide 'spookfox)
 ;;; spookfox.el ends here
