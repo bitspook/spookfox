57e48238d559e009bc9aae122301d7ae9a900e21
Charanjit Singh <bitspook@protonmail.com>
2022-10-23 13:30:54 +0530

    chore: release unlisted addon on github
    Even on manually uploading to addons.firefox.com, there is no option to make the
    addon publicly available. There seem to be no such option on their site; so fuck
    Firefox. They want to make developer-experience for their platform bad then it
    is on their head.

--- spookfox-addon/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((typescript-mode . ((eval .
-                           (add-to-list 'exec-path (concat (locate-dominating-file default-directory ".dir-locals.el") "node_modules/.bin/"))))))
